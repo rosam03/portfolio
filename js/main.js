9e856802e191d940c4e2383c5adc43bf75d9cec9
Rosa Mohammadi <rosamohammadi@hotmail.com>
2017-11-15 19:51:15 -0800

    cleaned up and commented, made responsiveness adjustments

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+
+/*
+ * Display the navigation menu drop down
+ */
+function display_dropdown() {
+	var elements = document.getElementsByClassName('dropdown')[0].getElementsByTagName('li');
+
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].style.visibility = "visible";
+		elements[i].style.display = "block";
+	}
+	document.getElementsByClassName('nav')[0].style.border = "none";
+}
+
+/*
+ * Hide the navigation menu drop down
+ */
+function hide_dropdown() {
+	var elements = document.getElementsByClassName('dropdown')[0].getElementsByTagName('li');
+
+	for (var i = 2; i < elements.length; i++) {
+		elements[i].style.visibility = "hidden";
+		elements[i].style.display = "none";
+	}
+	document.getElementsByClassName('nav')[0].style.border = "border-bottom: 1px solid #64B5F6";
+}
